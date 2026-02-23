db86fd5c3c0d6fe643365660065a11c1107f727d
maramreddipooja99 <maramreddi.Pooja@cognizant.com>
2026-02-23 10:09:27 +0530

    Delivery microservice

---
+++ Delivery/target/generated-sources/annotations/com/cts/mapper/DeviceMapperImpl.java
@@ -0,0 +1,53 @@
+package com.cts.mapper;
+
+import com.cts.dto.DeviceRequestDTO;
+import com.cts.dto.DeviceResponseDTO;
+import com.cts.model.Device;
+import com.cts.model.Device.DeviceType;
+import com.cts.model.Device.Status;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-02-20T17:41:15+0530",
+    comments = "version: 1.6.2, compiler: javac, environment: Java 21 (Oracle Corporation)"
+)
+@Component
+public class DeviceMapperImpl implements DeviceMapper {
+
+    @Override
+    public Device toEntity(DeviceRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Device device = new Device();
+
+        device.setUserId( dto.getUserId() );
+        device.setRegisteredDate( dto.getRegisteredDate() );
+
+        device.setStatus( Status.valueOf(dto.getStatus()) );
+        device.setDeviceType( DeviceType.valueOf(dto.getDeviceType()) );
+
+        return device;
+    }
+
+    @Override
+    public DeviceResponseDTO toDto(Device entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        DeviceResponseDTO deviceResponseDTO = new DeviceResponseDTO();
+
+        deviceResponseDTO.setUserId( entity.getUserId() );
+        deviceResponseDTO.setDeviceId( entity.getDeviceId() );
+        deviceResponseDTO.setRegisteredDate( entity.getRegisteredDate() );
+
+        deviceResponseDTO.setStatus( entity.getStatus() );
+        deviceResponseDTO.setDeviceType( entity.getDeviceType() );
+
+        return deviceResponseDTO;
+    }
+}
