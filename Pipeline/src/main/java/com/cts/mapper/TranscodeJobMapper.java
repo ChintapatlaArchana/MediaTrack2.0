ac8261dfe605d044e1e8bec9e3fcb9064af80dce
ChintapatlaArchana <Archana.Chintapatla@cognizant.com>
2026-02-25 12:21:43 +0530

    "Spring Config Added"

--- Pipeline/src/main/java/com/cts/mapper/TranscodeJobMapper.java
+++ Pipeline/src/main/java/com/cts/mapper/TranscodeJobMapper.java
@@ -6,19 +6,19 @@ import com.cts.model.TranscodeJob;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", imports = {TranscodeJob.TranscodeStatus.class})
+@Mapper(componentModel = "spring", imports = {TranscodeJob.TranscodeStatus.class,  java.time.LocalDate.class})
 public interface TranscodeJobMapper {
 
     @Mapping(target = "transcodeId", ignore = true)
-    @Mapping(target = "startedDate", expression = "java(java.time.LocalDateTime.now())")
-    @Mapping(target = "completedDate", ignore = true) // set later when job finishes
+    @Mapping(target = "startedDate", source = "startedDate", defaultExpression = "java(LocalDate.now())")
+    @Mapping(target = "completedDate", source = "completedDate", defaultExpression = "java(LocalDate.now())") // set later when job finishes
     @Mapping(target = "transcodeStatus", expression = "java(TranscodeStatus.Queued)") // default status
 //    @Mapping(target = "asset", ignore = true) // resolved in service layer using assetId
     TranscodeJob toEntity(TranscodeJobRequestDTO dto);
 
-    @Mapping(target = "transcodeStatus", expression = "java(entity.getTranscodeStatus())")
-    @Mapping(target = "startedDate", expression = "java(entity.getStartedDate())")
-    @Mapping(target = "completedDate", expression = "java(entity.getCompletedDate())")
+//    @Mapping(target = "transcodeStatus", expression = "java(entity.getTranscodeStatus())")
+//    @Mapping(target = "startedDate", expression = "java(entity.getStartedDate())")
+//    @Mapping(target = "completedDate", expression = "java(entity.getCompletedDate())")
 //    @Mapping(target = "assetId", source = "assetResponseDTO.assetId")
     TranscodeJobResponseDTO toDTO(TranscodeJob entity);
 }
